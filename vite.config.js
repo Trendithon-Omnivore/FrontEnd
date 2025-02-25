c3b8ad64738ca153bf97f99ef9a1b7d189f2b886
sinyoung6491 <sinyoung6491@gmail.com>
2025-02-25 23:18:25 +0900

    fix:https fix

--- vite.config.js
+++ vite.config.js
@@ -58,11 +58,11 @@ export default defineConfig(({ mode }) => {
     },
     server: process.env.NODE_ENV === 'development' ? {
       https:
-        //{
-        //key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-        //cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-        //}
-        false,
+        {
+        key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+        }
+        ,
       port: 5173,        // 강제로 5173 포트를 사용
       strictPort: true,
       proxy: {
