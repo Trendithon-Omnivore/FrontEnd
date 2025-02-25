f54d7821c86680119e077b648a998259408df7bf
taejun0 <xownswns@naver.com>
2025-02-25 17:52:26 +0900

    메인 페이지 구현 및 api 연결

--- src/components/layout/footer/mainfooter/MainFooter.jsx
+++ src/components/layout/footer/mainfooter/MainFooter.jsx
@@ -1,9 +1,32 @@
 import * as S from "./styled";
+import { useLocation } from "react-router-dom";
+import useCustomNavigate from "@hooks/useCustomNavigate";
+
+import house from "/images/main/house.svg";
+import history from "/images/main/history.svg";
+import my from "/images/main/my.svg";
+import houseactive from "/images/main/houseactive.svg";
+import historyactive from "/images/main/historyactive.svg";
+import myactive from "/images/main/myactive.svg";
 
 export const MainFooter = () => {
+  const location = useLocation();
+  const { goToPage } = useCustomNavigate();
   return (
     <S.Wrapper>
-      
+      <S.ImageWrap>
+        <S.IconBox onClick={() => goToPage("/history")} $isActive={location.pathname === "/history"}>
+          <S.Icon36 src={location.pathname === "/history" ? historyactive : history} />
+        </S.IconBox>
+
+        <S.IconBox onClick={() => goToPage("/main")} $isActive={location.pathname === "/main"}>
+          <S.Icon36 src={location.pathname === "/main" ? houseactive : house} />
+        </S.IconBox>
+
+        <S.IconBox onClick={() => goToPage("/mypage")} $isActive={location.pathname === "/mypage"}>
+          <S.Icon36 src={location.pathname === "/mypage" ? myactive : my} />
+        </S.IconBox>
+      </S.ImageWrap>
     </S.Wrapper>
   )
 }
--- src/components/layout/footer/mainfooter/styled.js
+++ src/components/layout/footer/mainfooter/styled.js
@@ -1,11 +1,39 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  position: absolute;
-  bottom: 0;
-  display: flex;
+  position: relative;
   width: 100%;
-  height: 83px;
+  height: 100px;
+  left: 0;
+  bottom: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   background: ${({ theme }) => theme.colors.white};
   box-shadow: 0px -4px 4px 0px rgba(0, 0, 0, 0.05);
+`;
+
+export const ImageWrap = styled.div`
+  display: flex;
+  width: 85%;
+  justify-content: space-between;
+`;
+
+export const IconBox = styled.div`
+  display: flex;
+  width: 32%;
+  height: 50px;
+
+  border-radius: 65px;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ theme, $isActive }) => $isActive ? theme.colors.black : theme.colors.lightbluegray};
+  cursor: pointer;
+`;
+
+export const Icon36 = styled.img`
+  width: 36px;
 `;
